85127f3814aed181b81bfd168f30ee9052a20074
Ho Hai Cong Thuan <hohaicongthuan@gmail.com>
2018-10-17 20:05:41 +0700

    Update tetris.cpp

---
+++ tetris.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+//==================================================
+//Khai báo các biến sử dụng trong chương trình
+int Score, HighScore;
+
+//==================================================
+//Các hàm sử dụng trong chương trình
+void ClearScreen()
+{
+
+}
+//==================================================
+void ScreenUpdater()
+{
+
+}
+//==================================================
+void BoardDrawer()
+{
+
+}
+//==================================================
+void MainMenu()
+{
+
+}
+//==================================================
+void Board1Update()
+{
+
+}
+//==================================================
+void Board2Update()
+{
+
+}
+
+//Đọc phím đã ấn từ bàn phím bằng hàm getch()
+
+//==================================================
+//Hàm main()
+int main()
+{
+
+}
