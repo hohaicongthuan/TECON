5f5a3ef646acef130a2fcc3fc51ccb2fcdfdff35
rock-on <36922874+fireser@users.noreply.github.com>
2018-11-05 19:14:47 +0700

    Update UI.cpp
    - Thêm màu cho chữ TETRIS.
    - Thêm nháy chữ.
    - Thêm câu lệnh chuyển từ giao diện Welcome sang giao diện Menu.

--- UI.cpp
+++ UI.cpp
@@ -1,13 +1,18 @@
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
 #include <iostream>
 
 using namespace std;
 
 void first_ui(int);
+void first_ui_black(int);
 void second_ui(int);
-
+char a[50][50];
 void first_ui(int)
 {
     //hàng đầu tiên
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(201);
     for(int i = 1; i <= 41; ++i)
         cout << char(205);
@@ -22,61 +27,96 @@ void first_ui(int)
     //hang thứ 2 (hàng chữ thứ 1)
     cout << char(186);
     cout << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << char(201) << char(205) << char(203) << char(205) << char(187) << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(201) << char(205) << char(205) << char(205) << char(187) << " "; //E
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(201) << char(205) << char(203) << char(205) << char(187) << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(201) << char(205) << char(205) << char(205) << char(187) << " "; //R
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << char(203) << " " << " ";                                       //I
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << char(201) << char(205) << char(205) << char(205) << char(187) << " "; //S
     cout << " " << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << endl;
 
     // hàng thứ 3 (hàng chữ thứ 2)
     cout << char(186);
     cout << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << " " << " " << " " << " " << " "; //E
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(186) << " " << " " << " " << char(186) << " "; //R
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << char(186) << " " << " "; // I
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << char(186) << " " << " " << " " << " " << " "; //S
     cout << " " << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << endl;
 
     // hàng thứ 4 (hàng chữ thứ 3)
     cout << char(186);
     cout << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(204) << char(205) << char(205) << char(205) << char(185) << " "; //E
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(204) << char(205) << char(203) << char(205) << char(188) << " "; //R
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << char(186) << " " << " "; // I
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << char(200) << char(205) << char(205) << char(205) << char(187) << " "; //S
     cout << " " << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << endl;
 
     //hàng thứ 5 (hàng chữ thứ 4)
     cout << char(186);
     cout << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << " " << " " << " " << " " << " "; //E
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << " " << " " << char(186) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(186) << " " << char(186) << " " << " " << " "; //R
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << char(186) << " " << " "; // I
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << " " << " " << " " << char(186) << " "; //S
     cout << " " << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << endl;
 
     //hàng thứ 6(hàng chữ thứ 5)
     cout << char(186);
     cout << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
     cout << " " << " " << char(202) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(200) << char(205) << char(205) << char(205) << char(188) << " "; //E
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << " " << " " << char(202) << " " << " " << " "; //T
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     cout << char(200) << " " << char(200) << char(205) << char(188) << " "; //R
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << " " << char(202) << " " << " "; // I
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
     cout << char(200) << char(205) << char(205) << char(205) << char(188) << " "; //S
     cout << " " << " " << " " << " ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(186) << endl;
 
     //hàng thứ 7 đến hàng thứ 14 (7 hàng)
@@ -89,9 +129,8 @@ void first_ui(int)
         }
     //hàng thứ 15
     cout << char(186);
-    cout << " " << " " << " " << " " << " " << " " << " ";
-    cout << "Press any key to continue";
-    cout << " " << " " << " " << " " << " " << " " << " " << " ";
+    for(int i = 1; i <= 40; ++i)
+        cout << " ";
     cout << " " << char(186) << endl;
 
     //hàng thứ 16 đến hàng thứ 20 (5 hàng)
@@ -108,11 +147,32 @@ void first_ui(int)
     for(int i = 1; i <= 41; ++i)
         cout << char(205);
     cout << char(188) << endl;
+
+    //ham tra con tro ve vi tri ghi chu "press any key to continue"
+    COORD p = { 8,15 };
+    SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), p );
+
+	//ham lam nhap nhay dong chu "press any key to continue"
+    Sleep(3000);
+    cout << "Press any key to continue";
+    Sleep(500);
+    for(int i = 0; i < 10; i++)
+    {
+        SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), p );
+        for(int j = 1; j <= 25; j++)
+            cout << " ";
+        SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), p );
+        Sleep(500);
+        cout << "Press any key to continue";
+        Sleep(500);
+    }
 }
 
 void second_ui(int)
 {
+    system("cls");
     //hàng đầu tiên
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
     cout << char(201);
     for(int i = 1; i <= 20; ++i)
         cout << char(205);
@@ -145,6 +205,14 @@ void second_ui(int)
 
 int main()
 {
+    //ham an con tro
+    CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+
     first_ui(1);
+    getch();
+    second_ui(1);
     return 0;
 }
