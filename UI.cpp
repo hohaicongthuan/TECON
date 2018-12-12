3fcb6d63568b7f0f37b5e9ef3f8c55657c77bdf6
rock-on <36922874+fireser@users.noreply.github.com>
2018-12-12 16:35:13 +0700

    Update UI.cpp
    Liên kết giữa Menu và Help, Credit, Exit.
    Thêm hàm xóa khung bên phải để phục vụ cho vẽ hàm help.
    Thêm hàm viết Credit (Thuận xem tui viết được không nè ~).
    Thêm hàm viết Help, hàm này để điều khiển những hàm help con (help(n)) (chưa viết xong nhưng cũng chạy ổn),
    Thêm break khi nhấn enter vô phần Exit.

--- UI.cpp
+++ UI.cpp
@@ -8,12 +8,18 @@ using namespace std;
 //mục lục hàm
 void gotoxy(int x,int y); //hàm di chuyển con trỏ đến vị trí (x,y)
 void khung(void); // hàm vẽ khung
+void xoakhungbenphai(void); //hàm xóa bên trong khung bên phải, thay vì vẽ lại cả khung thì chỉ cần xóa phần bên phải
 void welcome(void); // hàm vẽ  giao diện vào game
-void MenuLighting(void); //hàm chọn các option trong menu
-void KhungMenu(void);// vẽ hàm highlight chữ New Game
-void Menu2(void);// vẽ hàm highlight chữ Help
-void Menu3(void);// vẽ hàm highlight chữ Credit
-void Menu4(void);// vẽ hàm highlight chữ Exit
+void khungmenu(void);// hàm vẽ khung ban đầu khi mới vào giao diện menu
+void khungmenu1(void);// hàm highlight chữ New game
+void khungmenu2(void);// hàm highlight chữ Help
+void khungmenu3(void);// hàm highlight chữ Credit
+void khungmenu4(void);// hàm highlight chữ Exit
+void menulighting(void); //hàm chọn các option trong menu
+void help(void); //hàm để chọn các trang help
+void help1(void); //hàm viết trang help thứ nhất
+void help2(void); //hàm viết trang help thứ hai
+void credit(void); // hàm viết những người lập trình
 void gameover(void); // hàm vẽ giao diện kết thúc màn chơi
 //char a[50][50];
 
@@ -58,6 +64,16 @@ void khung(void)
     for(int i = 1; i <= 20; ++i)
         cout << char(205);
     cout << char(188) << endl;
+}
+
+void xoakhungbenphai(void)
+{
+    for(int j = 1; j <= 20; j++)
+    {
+        gotoxy(22,j);
+        for(int i = 22; i <= 41; i++)
+            cout << " ";
+    }
 }
 
 //giao dien welcome
@@ -221,10 +237,74 @@ void welcome(void)
 
 }
 
-void MenuLighting(void)
+//giao dien menu1
+void khungmenu(void)
+{
+    khung();
+    //cac option trong menu
+    gotoxy(7,6);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "New game";
+    gotoxy(7,9);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Help";
+    gotoxy(7,12);
+    cout << "Credit";
+    gotoxy(7,15);
+    cout << "Exit";
+}
+
+void khungmenu1(void)
+{
+    gotoxy(7,6);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "New game";
+    gotoxy(7,9);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Help";
+}
+
+void khungmenu2(void)
+{
+    gotoxy(7,6);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "New game";
+    gotoxy(7,9);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Help";
+    gotoxy(7,12);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Credit";
+}
+
+void khungmenu3(void)
+{
+    gotoxy(7,9);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Help";
+    gotoxy(7,12);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Credit";
+    gotoxy(7,15);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Exit";
+}
+
+void khungmenu4(void)
+{
+    gotoxy(7,12);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    cout << "Credit";
+    gotoxy(7,15);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    cout << "Exit";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+}
+//-----------------------------------------------------------------------------------Menu
+void menulighting(void)
 {
     system("cls");
-    KhungMenu();
+    khungmenu();
     char key;
     int ASCIIValue;
     int dem = 1;
@@ -235,130 +315,128 @@ void MenuLighting(void)
         {
             key = getch();
             ASCIIValue = key;
-            if(ASCIIValue == 97) dem--;
-            if(ASCIIValue == 100) dem++;
-            if(dem < 1)
+            if(ASCIIValue == 119) dem--; //coi mỗi giao diện là 1 con số, thay đổi con số là thay đổi giao diện
+            if(ASCIIValue == 115) dem++;
+            if(dem < 1) //chặn highlight nếu như đã ở trên cùng mà vẫn bấm nút lên
             {
                 dem = 1;
                 continue;
             }
-            if(dem > 4)
+            if(dem > 4) // chặn highlight nếu như đã ở dưới cùng mà vẫn bấm nút xuống
             {
                 dem = 4;
                 continue;
             }
             if(dem == 1)
             {
-                gotoxy(7,6);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "New game";
-                gotoxy(7,9);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Help";
+                khungmenu1(); //highlight chữ New game
+                //link phần của ông vào đây :3
             }
             if(dem == 2)
             {
-                gotoxy(7,6);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "New game";
-                gotoxy(7,9);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Help";
-                gotoxy(7,12);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Credit";
+                khungmenu2(); //highlight chữ Help
+                if(ASCIIValue == 13) help(); // nhảy vô giao diện help
+                if(ASCIIValue == 27) menulighting(); //thoát giao diện help
             }
             if(dem == 3)
             {
-                gotoxy(7,9);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Help";
-                gotoxy(7,12);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Credit";
-                gotoxy(7,15);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Exit";
+                khungmenu3(); //highlight chữ Credit
+                if(ASCIIValue == 13) credit(); //nhảy vô giao diện credit
+                if(ASCIIValue == 27) menulighting(); //thoát giao diện credit
             }
             if(dem == 4)
             {
-                gotoxy(7,12);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-                cout << "Credit";
-                gotoxy(7,15);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-                cout << "Exit";
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+                khungmenu4(); //highlight chữ Exit
+                if(ASCIIValue == 13) break; //thoát khỏi game
             }
         }
     }
 }
 
-//giao dien menu1
-void KhungMenu(void)
+void help(void)
 {
     khung();
-    //cac option trong menu
-    gotoxy(7,6);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "New game";
-    gotoxy(7,9);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Help";
-    gotoxy(7,12);
-    cout << "Credit";
-    gotoxy(7,15);
-    cout << "Exit";
+    help1();
+    char key;
+    int ASCIIValue;
+    int dem = 1;
+    while(1)
+    {
+        if(kbhit())
+        {
+            key = getch();
+            ASCIIValue = key;
+            if(ASCIIValue == 97) dem--;
+            if(ASCIIValue == 100) dem++;
+            if(dem < 1) //chặn highlight nếu như đã ở trên cùng mà vẫn bấm nút lên
+            {
+                dem = 1;
+                continue;
+            }
+            if(dem > 4) // chặn highlight nếu như đã ở dưới cùng mà vẫn bấm nút xuống
+            {
+                dem = 4;
+                continue;
+            }
+            if(dem == 1)
+            {
+                xoakhungbenphai();
+                help1();
+                if(ASCIIValue == 27) menulighting(); //thoát giao diện help
+            }
+            if(dem == 2)
+            {
+                xoakhungbenphai();
+                help2();
+                if(ASCIIValue == 27) menulighting(); //thoát giao diện credit
+            }
+        }
+    }
 }
 
-void Menu2(void)
+void help1(void)
 {
-    khung();
-    //cac option trong menu
-    gotoxy(7,6);
-    cout << "New game";
-    gotoxy(7,9);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Help";
-    gotoxy(7,12);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Credit";
-    gotoxy(7,15);
-    cout << "Exit";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    gotoxy (22,2); cout << "HOW TO PLAY:";
+    gotoxy (22,4); cout << "Your objective is to";
+    gotoxy (22,5); cout << "earn highest scores";
+    gotoxy (22,6); cout << "as long as possible";
+    gotoxy (22,7); cout << "by destroying";
+    gotoxy (22,8); cout << "Tetromino blocks.";
+    gotoxy (22,17); cout << "*Press d to continue";
+    gotoxy (22,18); cout << "*Press Esc to go";
+    gotoxy (22,19); cout << "back to menu";
 }
 
-void Menu3(void)
+void help2(void)
 {
-    khung();
-    //cac option trong menu
-    gotoxy(7,6);
-    cout << "New game";
-    gotoxy(7,9);
-    cout << "Help";
-    gotoxy(7,12);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Credit";
-    gotoxy(7,15);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Exit";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    gotoxy (22,2); cout << "To destroy the";
+    gotoxy (22,3); cout << "blocks, please place";
+    gotoxy (22,4); cout << "the blocks to make a";
+    gotoxy (22,5); cout << "full blocks row, and";
+    gotoxy (22,6); cout << "then the blocks row";
+    gotoxy (22,7); cout << "will be disappeared";
+    gotoxy (22,8); cout << "and you will earn ";
+    gotoxy (22,9); cout << "scores.";
+    gotoxy (22,17); cout << "*Press d to continue";
+    gotoxy (22,18); cout << "*Press a to go back";
+    gotoxy (22,19); cout << "*Press Esc to go";
+    gotoxy (22,20); cout << "back to menu";
 }
 
-void Menu4(void)
+//-------------------------------------------------------------------------------
+void credit(void)
 {
     khung();
-    //cac option trong menu
-    gotoxy(7,6);
-    cout << "New game";
-    gotoxy(7,9);
-    cout << "Help";
-    gotoxy(7,12);
-    cout << "Credit";
-    gotoxy(7,15);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
-    cout << "Exit";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN  | FOREGROUND_INTENSITY);
+    gotoxy(22,2); cout << "  This is project";
+    gotoxy(22,3); cout << "   homework in a";
+    gotoxy(22,4); cout << "  university class.";
+    gotoxy(22,6); cout << "Class:";
+    gotoxy(22,7); cout << "IT001.J13.MTCL - VN";
+    gotoxy(22,9); cout << "Programer:";
+    gotoxy(22,10); cout << "Ho Hai Cong Thuan";
+    gotoxy(22,11); cout << "ID: ";
+    gotoxy(22,12); cout << "Ha Kieu Trang";
+    gotoxy(22,13); cout << "ID: 18521522";
 }
 
 void gameover(void)
@@ -539,28 +617,6 @@ void gameover(void)
 
 }
 
-/*
-    khung(1);
-    int dem = 0;
-    gotoxy (22,2); cout << "HOW TO PLAY:";
-    gotoxy (22,4); cout << "Your objective is to";
-    gotoxy (22,5); cout << "earn highest scores as";
-    gotoxy (22,6); cout << "long as possible by";
-    gotoxy (22,7); cout << "destroying Tetromino";
-    gotoxy (22,8); cout << "blocks.";
-    gotoxy (22,18); cout << "Press d to continue";
-
-    gotoxy (22,2); cout << "To destroy the";
-    gotoxy (22,3); cout << "blocks, please place";
-    gotoxy (22,4); cout << "the blocks to make a";
-    gotoxy (22,5); cout << "full blocks row, and";
-    gotoxy (22,6); cout << "then the blocks row";
-    gotoxy (22,7); cout << "will be disappeared";
-    gotoxy (22,8); cout << "and you will earn ";
-    gotoxy (22,9); cout << "scores.";
-    gotoxy (22,18); cout << "Press d to continue";
-*/
-
 int main()
 {
     //ham an con tro
@@ -568,12 +624,9 @@ int main()
 	Info.bVisible = FALSE;
 	Info.dwSize = 20;
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
-
-    welcome();
-//    getch();
-//
-    MenuLighting();
-//    khung();
+//    welcome();
+    menulighting();
+//    credit();
 //    gameover();
     gotoxy(0,22);
     return 0;
